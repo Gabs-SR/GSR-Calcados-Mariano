952826501799f90d33f807fae81002da8f9f1e85
GabsSR <gabriel280207@gmail.com>
2026-08-23 15:26:22 -0300

    Protege escrita do catálogo com sessão

--- src/routes/produtoRoutes.js
+++ src/routes/produtoRoutes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const ProdutoController = require('../controllers/ProdutoController');
+const { exigirSessao } = require('../middlewares/autenticacao');
 
 const router = express.Router();
 
-// A rota de busca vem antes de /produtos/:id para nunca ser interpretada como id.
 router.get('/produtos/buscar', ProdutoController.buscarProdutos);
 router.get('/produtos/categorias', ProdutoController.listarOpcoesDeFiltro);
 router.get('/produtos', ProdutoController.listarProdutos);
 router.get('/produtos/:id', ProdutoController.mostrarProduto);
-router.post('/produtos', ProdutoController.adicionarProduto);
-router.put('/produtos/:id', ProdutoController.atualizarProduto);
-router.delete('/produtos/:id', ProdutoController.removerProduto);
+router.post('/produtos', exigirSessao, ProdutoController.adicionarProduto);
+router.put('/produtos/:id', exigirSessao, ProdutoController.atualizarProduto);
+router.delete('/produtos/:id', exigirSessao, ProdutoController.removerProduto);
 
 module.exports = router;
